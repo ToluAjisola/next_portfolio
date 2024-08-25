fe2b156e45720d319e8519f6d8dd012dcd64b6ad
ToluAjisola <toluwalopeajisola@gmail.com>
2024-08-24 23:08:22 -0400

    About section images added

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -5,21 +5,33 @@ const Grid = () => {
   return (
     <section id="about">
       <BentoGrid className="w-full py-20 grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto">
-        {gridItems.map((item, i) => (
-          <BentoGridItem
-            id={item.id}
-            key={i}
-            title={item.title}
-            description={item.description}
-            // remove icon prop
-            // remove original classname condition
-            className={item.className}
-            img={item.img}
-            imgClassName={item.imgClassName}
-            titleClassName={item.titleClassName}
-            spareImg={item.spareImg}
-          />
-        ))}
+        {gridItems.map(
+          ({
+            className,
+            id,
+            title,
+            description,
+            //   remove unecessary things here
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              // remove icon prop
+              // remove original classname condition
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
       </BentoGrid>
     </section>
   );
